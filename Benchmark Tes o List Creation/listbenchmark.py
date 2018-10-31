e347bbb7aca7b4eae3b15daa03940be721747ccf
ypraw <dyo.ndutz@gmail.com>
2018-10-31 13:17:39 +0700

    minor add syntax

--- Benchmark Tes o List Creation/listbenchmark.py
+++ Benchmark Tes o List Creation/listbenchmark.py
@@ -1,10 +1,12 @@
 import timeit
+import time
 from collections import OrderedDict
 import numpy as np
 
-number=5000
 
-
+start_time=time.time()
+number=2000
+item=1000
 def backInsert(item):
     i=0
     mylist=[]
@@ -12,8 +14,7 @@ def backInsert(item):
       mylist.insert(len(mylist),i)
       i+=1
     return mylist
-
-
+    
 def frontInsert(item):
     i=0
     mylist=[]
@@ -33,17 +34,19 @@ def listappend(item):
 def listnumpy(item):
     mylist=np.arange(item)
     return mylist
-#print(badInsert(1000))
-#print(reverseListInsert(1000))
 
-time1=timeit.timeit("backInsert(1000)",globals=globals(),number=number)
-time2=timeit.timeit("frontInsert(1000)",globals=globals(),number=number)
-time3=timeit.timeit("[x for x in range(1000)]",globals=globals(),number=number)
-time4=timeit.timeit("listappend(1000)",globals=globals(),number=number)
-time5=timeit.timeit("listnumpy(1000)",globals=globals(),number=number)
+#print(backInsert(1000))
+#print(frontInsert(1000))
+time1=timeit.timeit("backInsert(item)",globals=globals(),number=number)
+time2=timeit.timeit("frontInsert(item)",globals=globals(),number=number)
+time3=timeit.timeit("[x for x in range(item+1)]",globals=globals(),number=number)
+time4=timeit.timeit("listappend(item)",globals=globals(),number=number)
+time5=timeit.timeit("listnumpy(item)",globals=globals(),number=number)
 res=dict(back_insert=time1 ,front_insert=time2,list_compre=time3,list_append=time4,numpy_array=time5)
 res=OrderedDict(sorted(res.items(),key=lambda x:x[1]))
-print(f"LIST CREATION BENCHMARKING TEST\n*> every method generate 1000 items\n*> {number} loops for testing\n\n")
+print(f"Simple Benchmark Python List Function\n*> every method generate {item} items\n*> {number} time number executions loops\n\n")
 for k,v in res.items():
     print(f'{k} : {v:.5f} seconds')
+    
+print(f'\ntime execution {time.time()-start_time}')
 #print(f'from back: {time1:.3f} \nfrom front: {time2:.3f} \nlist compre: {time3:.3f} \nappend: {time4:.3f} \nnumpy array: {time5:.3f}')
